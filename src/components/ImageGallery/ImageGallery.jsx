35490649eb6280f4641406977e48011f7d347cd0
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2022-12-11 01:07:47 +0200

    ttt

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,25 +1,34 @@
 import { Component } from 'react';
+import axios from 'axios';
+import { ImageItem } from 'components/ImageGalleryItem/ImageItem';
 
+axios.defaults.baseURL = 'https://pixabay.com/api/?';
 const API_KEY = 'key=30638749-d35f41ebb6e3ac5e796b8db8e';
-const API_URL = 'https://pixabay.com/api/?';
 const DEFAULT_OPTIONS = 'image_type=photo&orientation=horizontal&per_page=12';
+
 export class ImageGallery extends Component {
   state = {
     page: 1,
+    totalPages: 0,
+    data: [],
   };
 
-  componentDidUpdate(prevProps) {
+  async componentDidUpdate(prevProps) {
     if (prevProps.searchQuery !== this.props.searchQuery) {
-      fetch(
-        `${API_URL}q=${this.props.searchQuery}&page=${this.state.page}&${API_KEY}&${DEFAULT_OPTIONS}`
+      const { searchQuery } = this.props;
+      const { page } = this.state;
+
+      const { data } = await axios.get(
+        `/q=${searchQuery}&page=${page}&${API_KEY}&${DEFAULT_OPTIONS}`
       );
+      this.setState({ data: data.hits });
     }
   }
 
   render() {
     return (
       <ul className="gallery">
-        <li></li>
+        <ImageItem data={this.state.data}></ImageItem>
       </ul>
     );
   }
